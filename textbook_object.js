087117ecf5a29e8775ca569481de1a966dbba8d6
ahtakle <aht6fb@virginia.edu>
2018-02-22 22:03:44 -0500

    Add 'Post Textbook' page

---
+++ textbook_object.js
@@ -0,0 +1,38 @@
+function Textbook (name, author, courseName, professor, price, type, condition, edition, isbn) {
+  this.name = name;
+  this.author = author;
+  this.courseName = courseName;
+  this.professor = professor;
+  this.price = price;
+  this.type = type;
+  this.condition = condition;
+  this.edition = edition;
+  this.isbn = isbn;
+  this.toString = function () {
+    var str = this.name + ", " + this.author + ", " + this.courseName + ", " + this.professor + ", " +
+      this.price + ", " + this.type + ", " + this.condition + ", " + this.edition + ", " + this.isbn;
+    return str;
+  };
+}
+
+var textbookList = [];
+
+function addTextbook() {
+  var typeElement = document.getElementById("type");
+  var type = typeElement.options[typeElement.selectedIndex].text;
+
+  var condElement = document.getElementById("condition");
+  var condition = condElement.options[condElement.selectedIndex].text;
+
+  textbookList[textbookList.length] = new Textbook(document.getElementById("name").value,
+    document.getElementById("author").value,
+    document.getElementById("coursename").value,
+    document.getElementById("professor").value,
+    document.getElementById("price").value,
+    type,
+    condition,
+    document.getElementById("edition").value,
+    document.getElementById("isbn").value);
+
+
+}
